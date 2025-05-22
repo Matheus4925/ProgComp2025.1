f338a0228aa1487ca88b2094c6d51f3192b83ac3
Matheus4925 <matheusneide4@gmail.com>
2025-05-22 10:25:56 -0300

    Update q2.py

--- AvaliativaComandoCondicional/q2.py
+++ AvaliativaComandoCondicional/q2.py
@@ -1,18 +1,17 @@
-nota1 = int(input("Insira a sua nota no 1º bimestre:"))
-nota2 = int(input("Insira a sua nota no 2º bimestre:"))
+nota1 = int(input("Digite a sua nota no 1º bimestre:"))
+nota2 = int(input("Digite a sua nota no 2º bimestre:"))
 
-md = (2 * nota1 + 3 * nota2)// 5
-if 20 <= md < 60:
+média = (2 * nota1 + 3 * nota2)// 5
+
+if média > 60:
+    print("Você foi aprovado.")
+elif 20 <= média < 60:
     print("Você realizou a prova final.")
-    notaf = int(input("Insira sua nota na prova final:"))
-    mdf = (md + notaf)// 2
-    if mdf < 60:
+    nota_final = int(input("Digite sua nota na prova final:"))
+    média_final = (média + nota_final)// 2
+    if média_final < 60:
         print("Você foi reprovado")
     else:
         print("Você foi aprovado.")
 else:
-    if 20 > md:
-        print("Você foi reprovado")
-    if md >= 60:
-        print("Você foi aprovado.")
-
+    print("Você foi reprovado.")
