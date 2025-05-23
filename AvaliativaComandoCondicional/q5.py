8e55a839bcb04d9ae91658f06dcaa7e1d8439cd2
Matheus4925 <matheusneide4@gmail.com>
2025-05-23 12:25:37 -0300

    Update q5.py

--- AvaliativaComandoCondicional/q5.py
+++ AvaliativaComandoCondicional/q5.py
@@ -1,10 +1,12 @@
 tempo = int(input("Informe o tempo estacionado em minutos:"))
 
+# Verificação se há fração de hora
 if tempo % 60 == 0:
     tempo = tempo // 60
 else:
     tempo = tempo // 60 + 1
 
+# Cálculo do valor total
 if tempo <= 1:
     valor = 8
     print(f"Você irá pagar R${valor}.")
