ad07ab7d1ac73cd1739e040d21e47957595c1ce3
Matheus4925 <matheusneide4@gmail.com>
2025-05-23 12:25:05 -0300

    Update q1.py

--- AvaliativaComandoCondicional/q1.py
+++ AvaliativaComandoCondicional/q1.py
@@ -1,9 +1,11 @@
 n = int(input("Digite um número de até 4 algarismos:"))
 soma = 0
 
+# Verifica se o número tem mais de 4 algarismos
 if n < 1 or n > 9999:
-    print("Esse número possui mais de 4 algarismos.")
-    
+    print("Esse número possui mais de 4 algarismos.")    
+
+# Calcula cada um dos algarismos até o número chegar em 0
 else:
     n != 0
     r = n % 10
