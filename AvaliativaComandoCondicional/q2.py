c03b173880344828e00e158ba28db8595cd1bdb4
Matheus4925 <matheusneide4@gmail.com>
2025-05-23 13:07:34 -0300

    Update q2.py

--- AvaliativaComandoCondicional/q2.py
+++ AvaliativaComandoCondicional/q2.py
@@ -2,7 +2,7 @@
 nota1 = int(input("Digite a sua nota no 1º bimestre:"))
 nota2 = int(input("Digite a sua nota no 2º bimestre:"))
 
-# Cálculo para a nota do aluno
+# Cálculo da média do aluno
 média = (2 * nota1 + 3 * nota2)// 5
 
 # Verificação da situação do aluno
